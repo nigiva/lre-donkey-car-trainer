597ad8248d75eb8ac491ed5950c11dd6989b27bc
Corentin Duchêne <corentin.duchene@epita.fr>
2021-08-24 14:54:17 +0200

    feat: nodebug: DCTrainer_V5

---
+++ src/dctrainer/model/model_wrapper.py
@@ -0,0 +1,32 @@
+import os
+from dctrainer.utils.inspector import load_source
+
+class DCModelWrapper:
+    def input_transformer(self, request):
+        raise Exception("Unimplemented function !")
+
+    def input_preprocessing(self, inputs_tensor, num_parallel_calls=1):
+        raise Exception("Unimplemented function !")
+
+    def predict(self, request):
+        raise Exception("Unimplemented function !")
+
+    def output_transformer(self, output):
+        raise Exception("Unimplemented function !")
+
+    def save(self, path):
+        raise Exception("Unimplemented function !")
+
+    def compile_model(self):
+        raise Exception("Unimplemented function !")
+
+    def load_model(self, model_path):
+        raise Exception("Unimplemented function !")
+
+    @staticmethod
+    def load(path, wrapper_filename="wrapper.code"):
+        wrapper_path = os.path.join(path, wrapper_filename)
+        LoadedBrain = load_source(wrapper_path, class_name_to_load="Brain")
+        loaded_brain_instance = LoadedBrain()
+        loaded_brain_instance.load_model(LoadedBrain.get_model_path(path))
+        return loaded_brain_instance
---
+++ src/dctrainer/utils/inspector.py
@@ -0,0 +1,59 @@
+import inspect
+from IPython.core.magics.code import extract_symbols
+import sys
+
+"""
+CC BY-SA 4.0
+Inspired by Evann Courdier and Nikita Kitaev
+https://stackoverflow.com/a/65806216
+"""
+
+def get_file_from_object(object, old_get_file=inspect.getfile):
+    """
+    Get __file__ from an object
+    Specially when it is not defined in using Jupyter notebook
+
+    :param object: the object
+    :param old_get_file: the old get_file function used by `inspect` lib
+    """
+    if not inspect.isclass(object):
+        return old_get_file(object)
+    
+    # Lookup by parent module (as in current inspect)
+    if hasattr(object, '__module__'):
+        object_ = sys.modules.get(object.__module__)
+        if hasattr(object_, '__file__'):
+            return object_.__file__
+    
+    # If parent module is __main__, lookup by methods (NEW)
+    for name, member in inspect.getmembers(object):
+        if inspect.isfunction(member) and object.__qualname__ + '.' + member.__name__ == member.__qualname__:
+            return inspect.getfile(member)
+    else:
+        raise TypeError('Source for {!r} not found'.format(object))
+
+inspect.getfile = get_file_from_object
+
+def save_source(source_class, path):
+  """
+  Save the source code of a class
+
+  :param source_class: class to save
+  :param path: filepath where the code will be saved
+  """
+  cell_code = "".join(inspect.linecache.getlines(get_file_from_object(source_class)))
+  class_code = extract_symbols(cell_code, source_class.__name__)[0][0]
+  with open(path, "w") as f:
+    f.write(class_code)
+
+def load_source(path, class_name_to_load="DCModel"):
+  """
+  Load the source code stored in a file
+  :param path: the code filepath
+  :param class_name_to_load: the name of the class to load (and return)
+  :return: the class entity of `class_name_to_load` loaded
+  """
+  g = dict(locals(), **globals())
+  with open(path, "r") as f:
+      exec(f.read(), g, g)
+  return g[class_name_to_load]
